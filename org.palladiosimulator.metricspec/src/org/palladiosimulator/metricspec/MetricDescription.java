0d5efe4a6c13659c8ca04de135a7c8bb88d955fe
snowball77 <steffen.becker@informatik.uni-stuttgart.de>
2014-04-26 09:29:34 +0000

    Moved all parts of the EDP2 MM which seved as metric specification into own plugin
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/MetricSpecification/trunk@22080 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.palladiosimulator.metricspec/src/org/palladiosimulator/metricspec/MetricDescription.java
@@ -0,0 +1,17 @@
+/**
+ */
+package org.palladiosimulator.metricspec;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Metric Description</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.palladiosimulator.metricspec.MetricSpecPackage#getMetricDescription()
+ * @model abstract="true"
+ * @generated
+ */
+public interface MetricDescription extends Description {
+} // MetricDescription
