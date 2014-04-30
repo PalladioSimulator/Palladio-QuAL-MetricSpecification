61d72d4497427a0e1a35417ae1fdfc664ad270f9
lehrig <sebastian.lehrig@informatik.tu-chemnitz.de>
2014-04-30 15:04:51 +0000

    Added cloud metrics to constants
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/MetricSpecification/trunk@22182 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

--- org.palladiosimulator.metricspec/src-man/org/palladiosimulator/metricspec/constants/MetricDescriptionConstants.java
+++ org.palladiosimulator.metricspec/src-man/org/palladiosimulator/metricspec/constants/MetricDescriptionConstants.java
@@ -37,6 +37,14 @@ public final class MetricDescriptionConstants {
     	PASSIVE_RESOURCE_STATE_METRIC = (BaseMetricDescription) resource.getEObject("_x0-pks7rEeOX_4BzImuHbA");
     	EXECUTION_RESULT_METRIC = (BaseMetricDescription) resource.getEObject("_7Is3ss7rEeOX_4BzImuHbA");
     	CPU_STATE_OVER_TIME_METRIC = (MetricSetDescription) resource.getEObject("_MSlw0c7sEeOX_4BzImuHbA");
+    	SCALABILITY_RANGE = (BaseMetricDescription) resource.getEObject("_gNxNEtBxEeONzY86HEL4JQ");
+    	SLOPE = (BaseMetricDescription) resource.getEObject("_Nz_HctBzEeONzY86HEL4JQ");
+    	SCALABILITY_SPEED = (MetricSetDescription) resource.getEObject("_kx_7UdBzEeONzY86HEL4JQ");
+    	USER_CAPACITY = (BaseMetricDescription) resource.getEObject("_HlBOUtB0EeONzY86HEL4JQ");
+    	NUMBER_OF_SLO_VIOLATIONS = (BaseMetricDescription) resource.getEObject("_LJJm8tB1EeONzY86HEL4JQ");
+    	MEAN_TIME_TO_QUALITY_REPAIR = (BaseMetricDescription) resource.getEObject("_8m5pAtB1EeONzY86HEL4JQ");
+    	RESOURCE_PROVISIONING_EFFICIENCY = (BaseMetricDescription) resource.getEObject("_RjNq4tB2EeONzY86HEL4JQ");
+    	MARGINAL_COST = (BaseMetricDescription) resource.getEObject("_eQICYtB2EeONzY86HEL4JQ");
     }
 	
     /** Specifies a response time metric, e.g., to store the response time of operation calls. */
@@ -64,6 +72,30 @@ public final class MetricDescriptionConstants {
     public final static BaseMetricDescription EXECUTION_RESULT_METRIC;
 
     public final static MetricSetDescription CPU_STATE_OVER_TIME_METRIC;
+    
+    /** Scalability range without violating SLOs, e.g., the system scales up to 1,000 users. */
+    public final static BaseMetricDescription SCALABILITY_RANGE;
+    
+    /** The slope for increase rates, e.g., an increase rate of 10 users/hour. */
+    public final static BaseMetricDescription SLOPE;
+    
+    /** Scalability speed without violating SLOs, e.g., the system scales up to 1,000 users with increase rate 10 users/hour. */
+    public final static MetricSetDescription SCALABILITY_SPEED;
+    
+    /** Maximal number of users for fixed resource without violating SLOs, e.g., a system scales up to 1,000 users for a fixed set of resources. */
+    public final static BaseMetricDescription USER_CAPACITY;
+    
+    /** The number of SLO violations in a certain time frame, e.g., 42 SLO (response time) violations in 1 hour. */
+    public final static BaseMetricDescription NUMBER_OF_SLO_VIOLATIONS;
+    
+    /** The mean time to stop violating SLOs, e.g., 30 seconds fir an additional 10 requests/hour. */
+    public final static BaseMetricDescription MEAN_TIME_TO_QUALITY_REPAIR;
+    
+    /** Efficiency of provisioning resources, e.g., 10% more resources than actual resource demand. */
+    public final static BaseMetricDescription RESOURCE_PROVISIONING_EFFICIENCY;
+    
+    /** Marginal cost for serving additional load, e.g., $1.00 for an additional 100 requests/hour. */
+    public final static BaseMetricDescription MARGINAL_COST;
 
     private MetricDescriptionConstants() {
     }
